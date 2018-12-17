c914881b725da3539b17446b0bc65778df3615ec
Andrew Tang <andrew.tang@outlook.com>
2018-12-17 15:52:10 +1000

    Seperate play controls and add title

---
+++ src/PlayControls.js
@@ -0,0 +1,64 @@
+import React from "react";
+import style from "./Controls.scss";
+import Button from "./Button";
+import { ResetIcon, RunIcon } from "./Icons";
+
+const PlayControls = ({
+    startPressed,
+    startDisabled,
+    run,
+    resetDisabled,
+    stop,
+    intervalDuration,
+    handleIntervalChange
+}) => {
+    function onBlur(event) {
+        let { value } = event.target;
+        value = value < 100 ? 100 : value;
+        this.props.changeIntervalDuration(value);
+    }
+
+    return (
+        <div>
+            <div style={{ paddingLeft: "13px", fontSize: "15px" }}>
+                Controls
+            </div>
+            <div className={style["running-controls"]}>
+                <div className={style["toggle"]}>
+                    <Button
+                        title="[P]lay"
+                        pressed={startPressed}
+                        disabled={startDisabled}
+                        onClick={run}
+                    >
+                        <RunIcon disabled={startDisabled} />
+                    </Button>
+
+                    <Button
+                        title="[R]eset"
+                        disabled={resetDisabled}
+                        onClick={stop}
+                    >
+                        <ResetIcon disabled={resetDisabled} />
+                    </Button>
+                </div>
+                <div>
+                    <div style={{ fontSize: "12px" }}>Cycle Every</div>
+                    <input
+                        className={style["interval"]}
+                        type="number"
+                        value={intervalDuration}
+                        onChange={handleIntervalChange}
+                        min={100}
+                        max={9999}
+                        step={10}
+                        onBlur={onBlur}
+                    />
+                    <span> ms</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PlayControls;
